c8a5fe27afccc3a8678857ccae6cf3569f512407
haeuuu <srhuu15@naver.com>
2020-12-15 19:47:28 +0900

    fix : get_data에 arena_data 구조 수정 사항 반영

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import json
-import os
+import os, io
+import numpy as np
 
 def load_json(fname):
     with open(fname, encoding="utf-8") as f:
@@ -7,17 +8,27 @@ def load_json(fname):
 
     return json_obj
 
-def get_data(dir, test = True):
-    if test:
-        train_path = os.path.join(dir, 'org/train.json')
-        val_path = os.path.join(dir, 'qustions/val_questions.json')
-    else:
-        pass
+def write_json(data, fname):
+    def _conv(o):
+        if isinstance(o, (np.int64, np.int32)):
+            return int(o)
+        raise TypeError
 
-    base_path = os.path.join(dir, 'base_results_gep.json')
+    with io.open(fname, "w", encoding="utf-8") as f:
+        json_str = json.dumps(data, ensure_ascii=False, default=_conv)
+        f.write(json_str)
+
+def get_data(dir = 'arena_data', test = True):
+    if test:
+        train_path = os.path.join(dir, 'orig' ,'train.json')
+        val_que_path = os.path.join(dir, 'questions','val_questions.json')
+        val_ans_path = os.path.join(dir, 'answers','val_answers.json')
+    else:
+        train_path = os.path.join(dir, 'submission' ,'train.json')
+        val_path = os.path.join(dir, 'submission','val_answers.json')
 
     train = load_json(train_path)
-    val = load_json(val_path)
-    base_res = load_json(base_path)
+    val_que = load_json(val_que_path)
+    val_ans = load_json(val_ans_path)
 
-    return train, val, base_res
+    return train, val_que, val_ans
