7e155e78ae0d44c7fec57745196d211f65372570
haeuuu <srhuu15@naver.com>
2021-01-31 01:32:30 +0900

    refactor : vocab과 w2v model을 모두 받을 수 있도록

--- weighted_ratings.py
+++ weighted_ratings.py
@@ -2,7 +2,6 @@ import copy
 import numpy as np
 from itertools import chain
 from scipy.sparse import coo_matrix, csr_matrix
-from math import log
 
 class Ratings:
     """
@@ -83,14 +82,22 @@ class Node:
         self.children = {}
         self.is_terminal = False
 
-class Filter:
-    def __init__(self, items):
-        self.items = items
+class TagExtractor:
+    def __init__(self):
         self.head = Node(None)
 
+    def build_by_vocab(self, words):
         print("DB 구성중입니다 ...")
-        for item in items:
-            self.insert(item)
+        for word in words:
+            self.insert(word)
+        print("입력 완료 ...")
+
+    def build_by_w2v(self, w2v_model):
+        print("DB 구성중입니다 ...")
+        for word in w2v_model.wv.vocab.keys():
+            if word.isdigit():
+                continue
+            self.insert(word)
         print("입력 완료 ...")
 
     def insert(self, query):
@@ -102,12 +109,25 @@ class Filter:
             curr_node = curr_node.children[q]
         curr_node.is_terminal = query
 
+    def search(self, query):
+        curr_node = self.head
+
+        for q in query:
+            curr_node = curr_node.children.get(q)
+            if curr_node is None:
+                return False
+
+        if curr_node.is_terminal:
+            return True
+        return False
+
     def extract(self, query, biggest_token = True):
         """
         :param query: str(word)
         :param biggest_token: True인 경우, word에서 찾을 수 있는 가장 큰 tag를 return.
         :return: list of tags
         """
+
         start, end = 0,0
         query += '*'
         curr_node = self.head
@@ -133,13 +153,26 @@ class Filter:
 
         return  extracted_tags
 
-    def extract_from_title(self, title, biggest_token = True):
+    def extract_from_title(self, title, biggest_token = True, nouns = False):
         """
         :param title: str(title)
         :param biggest_token: True인 경우, word에서 찾을 수 있는 가장 큰 tag를 return.
         :return: list of tags
         """
+
         tags = []
-        for word in title.split():
-            tags.extend(self.extract(word,biggest_token))
+        if nouns:
+            return self.extract_nouns(title)
+        else:
+            for word in title.split():
+                tags.extend(self.extract(word,biggest_token))
+            return tags
+
+    def extract_nouns(self, title):
+        nouns = hannanum.nouns(" ".join(self.resub(title)))
+        tags = []
+        for noun in nouns:
+            if self.search(noun):
+                tags.append(noun)
+
         return tags
