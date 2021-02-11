b2f09e0336acdee05077ffa685e076f0fb109372
haeuuu <srhuu15@naver.com>
2021-02-11 18:08:04 +0900

    remove : get_data 함수 삭제

--- util.py
+++ util.py
@@ -17,18 +17,3 @@ def write_json(data, fname):
     with io.open(fname, "w", encoding="utf-8") as f:
         json_str = json.dumps(data, ensure_ascii=False, default=_conv)
         f.write(json_str)
-
-def get_data(dir = 'arena_data', test = True):
-    if test:
-        train_path = os.path.join(dir, 'orig' ,'train.json')
-        val_que_path = os.path.join(dir, 'questions','val_questions.json')
-        val_ans_path = os.path.join(dir, 'answers','val_answers.json')
-    else:
-        train_path = os.path.join(dir, 'submission' ,'train.json')
-        val_path = os.path.join(dir, 'submission','val_answers.json')
-
-    train = load_json(train_path)
-    val_que = load_json(val_que_path)
-    val_ans = load_json(val_ans_path)
-
-    return train, val_que, val_ans
